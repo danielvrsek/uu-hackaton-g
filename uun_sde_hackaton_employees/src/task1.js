45d31d03f0ee1f3ec6dd4ca3ee7aaa0184315ee3
Tommee Colorado <t.klejna@gmail.com>
2021-01-16 10:25:44 +0100

    employees wip

--- uun_sde_hackaton_employees/src/task1.js
+++ uun_sde_hackaton_employees/src/task1.js
@@ -70,7 +70,27 @@ async function main() {
   await _validateDtoIn(dtoIn, uuAppErrorMap);
 
   /*@@viewOn:sourceCode*/
-  
+  const FEMALE_FIRSTNAMES = ["Marcela", "Petra", "Alice", "Helena", "Veronika", "Irena", "Olga", "Alena", "Marie", "Michaela", "Eva", "Jana", "Kristýna", "Lucie", "Lenka", "Jindra", "Vlasta", "Zdenka", "Dana", "Martina", "Věra", "Viera", "Marika", "Jaroslava", "Drahomíra", "Hana", "Anna", "Ludmila", "Barbora", "Jiřina", "Božena", "Diana", "Zdeňka", "Gabriela", "Dagmar", "Soňa", "Jitka", "Kateřina", "Zuzana", "Tereza", "Natálie", "Markéta", "Andrea", "Jarmila", "Daniela", "Vladimíra", "Dita", "Šárka", "Ivana", "Monika"];
+  const MALE_FIRSTNAMES = ["Vlastimil", "Roman", "Ladislav", "Josef", "Jan", "Miroslav", "Tomáš", "Jaroslav", "Jiří", "Václav", "Oldřich", "Karel", "Petr", "Zdeněk", "Leonardus Franciscus", "František", "Milan", "Pavel", "Lukáš", "Radek", "Rolf Harri", "Martin", "Vlastislav", "Daniel", "Bohuslav", "Lubomír", "Illya", "Ivan", "Michal", "Vladimír", "Vojtěch", "David", "Patrik", "Jaromír", "Aleš", "Bohdan", "Stanislav", "Radislav", "Antonín", "Leoš", "Ondřej", "Jakub", "Marek", "Vittorio", "Arnošt", "Robert", "Vítězslav", "Radomír", "Richard", "Vladislav"];
+  const FEMALE_LASTNAMES = ["Hubáčková", "Veselá", "Všetulová", "Šímová", "Beránková", "Jarolímková", "Šimková", "Kaňoková", "Horáková", "Gregárková", "Prchalová", "Blahová", "Melicharová", "Trutnovská", "Bulisová", "Lachmanová", "Kremlová", "Čejková", "Matušková", "Kolovecká", "Banasinská", "Kusáková", "Kitzbergerová", "Polívková", "Suchá", "Tomsová", "Šimáková", "Štěpánková", "Vlková", "Venclíčková", "Bartošová", "Ivanochko", "Kuzniková", "Bednaříková", "Gregorová", "Milerová", "Konečná", "Valešová", "Káčerková", "Culková", "Vebrová", "Rajmová", "Slaninová", "Kostomlatská", "Kašová", "Filipová", "Klodová", "Nováková", "Chrastinová", "Marková"];
+  const MALE_LASTNAMES = ["Stránský", "Bílek", "Preuss", "Kolečář", "Štuchal", "Šindelář", "Kindl", "Novotný", "Mžourek", "Chrastina", "Všetečka", "Bajaja", "Srnec", "Chrt", "Crhonek", "Šnajdr", "Pawlas", "Macela", "Bičiště", "Kos", "Maksimov", "Poula", "Andrlík", "Handl", "Janoušek", "Hýbl", "Havel", "Tichý", "Přikryl", "Kučera", "Vaško", "Fuksa", "Šandera", "Diviš", "Sedlák", "Pavlů", "Suk", "Foks", "Wiedermann", "Zvelebil", "Stráňka", "Zadražil", "Maděra", "Rýdl", "Staník", "Batlička", "Pavel", "Burian", "Beran", "Malyjurek"];
+
+
+  let employees = [];
+
+  let gendercode = 0;
+  for (let i = 0; i < dtoIn.count; i++) {
+    gendercode = _getRandom(0, 1);
+    employees.push({
+      gender: gendercode == 0 ? "male" : "female",
+      birthdate: 
+      name: gendercode == 0 ? MALE_FIRSTNAMES[_getRandom(0, MALE_FIRSTNAMES.length - 1)] : FEMALE_FIRSTNAMES[_getRandom(0, FEMALE_FIRSTNAMES.length - 1)],
+      surname: gendercode == 0 ? MALE_LASTNAMES[_getRandom(0, MALE_LASTNAMES.length - 1)] : FEMALE_LASTNAMES[_getRandom(0, FEMALE_LASTNAMES.length - 1)],
+      workload: _getRandom(1, 4) * 10
+    })
+  }
+
+
   /*@@viewOff:sourceCode*/
 
   return { uuAppErrorMap };
